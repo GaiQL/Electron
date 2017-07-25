256b31c157a38cc48413d92ba669a9ecd5525a78
GaiQL <609100252@qq.com>
2017-07-25 22:55:27 +0800

    1.4

--- music/js/2D.js
+++ music/js/2D.js
@@ -1,68 +1,77 @@
 var center = document.getElementById('center');
-var zz = document.getElementById('zz');
-center.style.top = innerHeight/2 - (center.offsetHeight/2) + 'px';
-center.style.left = innerWidth/2 - (center.offsetWidth/2) + 'px';
-zz.style.top = innerHeight/2 - (center.offsetHeight/2) + 'px';
-zz.style.left = innerWidth/2 - (center.offsetWidth/2) + 'px';
-var imgs = document.getElementsByTagName('img');
-var imgs1 = zz.getElementsByTagName('img');
+var tfg = document.getElementById('tfg');
+var tsg = document.getElementById('tsg');
+var ttg = document.getElementById('ttg');
 var num = 0;
-var n = 0; 
-for(var i=0;i<imgs.length;i++){
-	num = 360/imgs.length;
-	imgs[i].style.top = parseInt(getComputedStyle(center).top)-zz.offsetTop +'px';
-	imgs[i].style.left = parseInt(getComputedStyle(center).left)+((center.offsetWidth-imgs[i].offsetWidth)/2)-zz.offsetLeft+'px';
-//	imgs[i].style.transformOrigin = (imgs[i].offsetWidth/2)+'px '+(130+(center.offsetHeight/2))+'px';
-	imgs[i].style.transformOrigin = '105px 124px'
-	imgs[i].style.transform = 'rotate('+(num*i)+'deg)';
-	imgs[i].rotate = num*i;
+var timer = null;
+var c = 0;
+//初始化
+var t = innerHeight/2 - (center.offsetHeight/2);
+var l = innerWidth/2 - (center.offsetWidth/2);
+center.style.top = t + 'px';
+center.style.left = l + 'px';
+tfg.style.top = tsg.style.top = ttg.style.top = t + 'px';
+tfg.style.left = tsg.style.left = ttg.style.left = l + 'px';
+//for(var i=0;i<data[0].first.length;i++){
+//	num = 360/data[0].first.length;
+//	var div = document.createElement('div');
+//	div.className = 'tfg';
+//	div.style.backgroundImage = 'url('+data[0].first[i].img+')';
+//	div.style.transform = 'rotate('+(num*i)+'deg)';
+//	tfg.appendChild(div);
+//	div.style.top = parseInt(getComputedStyle(center).top)-tfg.offsetTop +'px';
+//	div.style.left = (center.offsetWidth-div.offsetWidth)/2+'px';
+//}
+//obj calssName n
+produce(data[0].first,'tfg');
+function produce(obj,Name){
+	for(var i=0;i<obj.length;i++){
+		num = 360/obj.length;
+		var div = document.createElement('div');
+		div.className = Name;
+		div.style.backgroundImage = 'url('+obj.img+')';
+		div.style.transform = 'rotate('+(num*i)+'deg)';
+		tfg.appendChild(div);
+		div.style.top = parseInt(getComputedStyle(center).top)-tfg.offsetTop+'px';
+		div.style.left = (center.offsetWidth-div.offsetWidth)/2+'px';
+	}
 }
-var timer = setTimeout(fn,1000)
+var divs_tfg = document.getElementsByClassName('tfg');
+var timer = setTimeout(beginshow,1000)
 var x = 0;
-function fn(){
+function beginshow(){
 	var s = 124;
 	var n = 0;
-
 	var timer2 = setInterval(function(){
-		if(s > (130+(center.offsetHeight/2))){
-			s = (130+(center.offsetHeight/2));
+		s+=4;
+		if(s >= (130+(center.offsetHeight/2))){
+
 			clearInterval(timer2);
+			s = (130+(center.offsetHeight/2));
+			
 		}
-		imgs[x].style.transformOrigin = '105px '+s+'px';
-		s+=5;
-	},20)
+		divs_tfg[x].style.transformOrigin = '105px '+s+'px';
+	},4)
 	var timer3 = setInterval(function(){
-		if(n < -130){
-			n = -130;
+		n-=4;
+		if(n <= -130){
 			clearInterval(timer3);
+			n = -130;	
 		}
-		imgs[x].style.top = n+'px';
-		n-=5;
-	},20)
+		divs_tfg[x].style.top = n+'px';
+	},4)
 	var timer4 = setInterval(function(){
-		if(s >= (130+(center.offsetHeight/2)) && n <= -130){
+		if(s == (130+(center.offsetHeight/2)) && n == -130){
 			clearInterval(timer4);
 			x++;
-			if(x<imgs.length){
-				fn();
+			if(x<divs_tfg.length){
+				beginshow();
+			}else{
+				timer = setInterval(function(){
+					tfg.style.transform = 'rotate('+c+'deg)';
+					c+=0.1;
+				},20);
 			}
 		}
-	},20)
-//	move({
-//		obj:imgs[x],
-//		attrs:{transformOringin:254,top:parseInt(getComputedStyle(center).top)-130-zz.offsetTop},
-//		duration:1000,
-//		callBack:function(){
-//				x++;
-//				if(x<imgs.length){
-//					fn();
-//				}
-//		}
-//	})
+	},4)
 }
-//var timer = setInterval(function(){
-//		zz.style.transform = 'rotate('+n+'deg)';
-//	n++;
-//},60)
-
-/*根据传入角度去定位，自动修改XY的值*/
